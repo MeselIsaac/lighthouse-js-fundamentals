f5e05e999997d7faa6b0bbdd17d2c334ebc58ff5
Mesel <meselisaac03@hotmail.com>
2019-02-19 18:30:38 +0000

    'LoopyLighthouse' assignment completed

--- loopylighthouse.js
+++ loopylighthouse.js
@@ -1,15 +1,3 @@
- /*for (var num = 100; num <= 200; num++) {
-  if (num % 3 === 0 && num % 4 === 0) {
-    console.log("LoopyLighthouse");
-  } else if (num % 4 === 0) {
-    console.log("Lighthouse");
-  } else if (num % 3 === 0) {
-    console.log("Loopy");
-  } else {
-    console.log(num);
-  }
-}*/
-
 for (var num = 100; num <= 200; num++) {
   if (num % 3 === 0 && num % 4 === 0) {
     console.log("LoopyLighthouse");
@@ -21,10 +9,3 @@ for (var num = 100; num <= 200; num++) {
     console.log(num);
   }
 }
-
-
-
- // console.log(num);
- //} if (num % 3 === 0) {
-  //console.log("Loopy");
- //}
